ceacc64af50dc41a237fa4101cf9a181faddaa62
dev-pedro <dev.pedro.rjas@gmail.com>
2023-03-13 11:40:53 -0300

    update project

--- script.js
+++ script.js
@@ -2,11 +2,91 @@ const form = document.querySelector('.quiz-form')
 const finalScoreContainer = document.querySelector('.final-score-container')
 const paragraphBruxoOuTrouxa = document.querySelector('.bruxo-trouxa')
 const divQuestion = document.querySelector('.questions')
-let correctAnswers = []
+let correctAnswers = ['2', '1', '0', '2', '0', '2', '1', '1', '3', '1']
 let scoreScript = 0
-let percentualAsked = 0
+let percentualAsked = Math.round(100 / correctAnswers.length)
 let wasAnswered = false
 let templateHTML = ''
+
+const questions = [
+  {
+    number: 1,
+    title: 'Qual é a varinha das varinhas?',
+    options: [
+      'Varinha de Harry Potter',
+      'Varinha de Lord Voldemort',
+      'Varinha de Alvo Dumbledore'
+    ],
+    correctAnswer: '2'
+  },
+  {
+    number: 2,
+    title: 'Qual é a casa de Hogwarts de Harry Potter?',
+    options: ['Grifinória', 'Sonserina', 'Lufa-Lufa', 'Corvinal'],
+    correctAnswer: '1'
+  },
+  {
+    number: 3,
+    title:
+      'Qual é o nome do diretor da Escola de Magia e Bruxaria de Hogwarts?',
+    options: [
+      'Alvo Dumbledore',
+      'Minerva McGonagall',
+      'Severo Snape',
+      'Dolores Umbridge'
+    ],
+    correctAnswer: '0'
+  },
+  {
+    number: 4,
+    title:
+      'Qual é a criatura que guarda a Pedra Filosofal em Harry Potter e a Pedra Filosofal?',
+    options: ['Dragão', 'Unicórnio', 'Cerbero', 'Fênix'],
+    correctAnswer: '2'
+  },
+  {
+    number: 5,
+    title: 'Quem é o melhor amigo de Harry Potter?',
+    options: [
+      'Ron Weasley',
+      'Hermione Granger',
+      'Draco Malfoy',
+      'Neville Longbottom'
+    ],
+    correctAnswer: '0'
+  },
+  {
+    number: 6,
+    title: 'Qual é o patrono de Harry Potter?',
+    options: ['Gato', 'Coruja', 'Cervo', 'Cão'],
+    correctAnswer: '2'
+  },
+  {
+    number: 7,
+    title:
+      'Qual é o feitiço que Harry Potter usa para derrotar Lord Voldemort no final da série?',
+    options: ['Avada Kedavra', 'Expelliarmus', 'Imperio', 'Crucio'],
+    correctAnswer: '1'
+  },
+  {
+    number: 8,
+    title: 'Qual é o nome do animal de estimação de Hermione Granger?',
+    options: ['Edwiges', 'Crookshanks', 'Pigwidgeon', 'Nagini'],
+    correctAnswer: '1'
+  },
+  {
+    number: 9,
+    title: 'Qual é o nome do irmão mais velho de Ron Weasley?',
+    options: ['Charlie', 'Percy', 'Fred', 'George'],
+    correctAnswer: '3'
+  },
+  {
+    number: 10,
+    title: 'Qual é o nome do elfo doméstico que ajuda Harry Potter?',
+    options: ['Kreacher', 'Dobby', 'Winky', 'Hokey'],
+    correctAnswer: '1'
+  }
+]
 
 /**Cria as perguntas com as opções dentro do form
  * esta função retorna um template HTML contendo
@@ -18,10 +98,7 @@ let templateHTML = ''
  */
 function insertAsketsOnIndexHTML (questions = {}) {
   let asksLabel = ''
-  questions.forEach(({ number, title, options, correctAnswer }) => {
-    //adiciona em um arrays as respostas corretas
-    correctAnswers.push(correctAnswer)
-
+  questions.forEach(({ number, title, options }) => {
     //cria as input label com as opções de respostas
     options.forEach((option, index) => {
       asksLabel += `<div class="form-check my-2 text-black-50">
@@ -46,13 +123,8 @@ function insertAsketsOnIndexHTML (questions = {}) {
   form.insertAdjacentElement('afterbegin', divQuestion)
   //return templateHTML
 }
-//carrega um JSON e chama function para popular o index
-fetch('./questions.json')
-  .then(response => response.json())
-  .then(data => {
-    insertAsketsOnIndexHTML(data.questions)
-    percentualAsked = Math.round(100 / data.questions.length)
-  })
+//chama a função para carregar os inputs no index
+insertAsketsOnIndexHTML(questions)
 
 //armazena as resposta do usuário no array 'userAnswers'
 const getUserAswers = () => {
@@ -87,10 +159,9 @@ const showFinalScore = () => {
   finalScoreContainer.classList.remove('d-none')
 }
 
-
 //mostra mensagem conforme a pontuação
 const showBruxoOuTrouxa = () => {
-  const showBruxoOuTrouxa = paragraphBruxoOuTrouxa.querySelector('span') 
+  const showBruxoOuTrouxa = paragraphBruxoOuTrouxa.querySelector('span')
   if (scoreScript >= 50) {
     showBruxoOuTrouxa.textContent = '" Bruxo "'
   } else {
@@ -112,7 +183,7 @@ const animateFinalScore = () => {
     finalScoreContainer.querySelector('span').textContent = `${counter++}%`
   }, 20)
 
-  wasAnswered = true//evita soma do resultado repetidas vezes
+  wasAnswered = true //evita soma do resultado repetidas vezes
 }
 
 //envia o form
